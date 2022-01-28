a17a45cfef68fee9d8309e2105c8095855291154
Tatsiana A <astrouskaya@yahoo.com>
2022-01-28 14:54:05 -0500

    add hooks

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -2,26 +2,41 @@ import {expect} from 'chai';
 import supertest from 'supertest';
 
 describe('auth', function () {
-    const request = supertest(process.env.BASE_URL)
-    it('success log in', function () {
-        request
-            .post('/auth')
-            .send({login: process.env.LOGIN, password: process.env.PASSWORD})
-            .expect(200)
-            .end(function (err, res) {
-                expect(res.statusCode).to.eq(200);
+    let result;
+
+    describe('success log in', function () {
+        before(function () {
+            result = supertest(process.env.BASE_URL)
+                .post('/auth')
+                .send({login: process.env.LOGIN, password: process.env.PASSWORD})
+        })
+
+        it('response status code 200', function () {
+            result.expect(200);
+        })
+
+        it('response contains token', function () {
+            result.end(function (err, res) {
                 expect(res.body.token).not.to.be.undefined;
             })
+        })
     })
 
-    it('log in with wrong credentials', function () {
-        request
-            .post('/auth')
-            .send({login: 'wrong', password: 'wrong'})
-            .expect(200)
-            .end(function (err, res) {
-                expect(res.statusCode).to.eq(404);
-                expect(res.body.message).to.eq('Wrong login or password.');
-            })
+    describe('log in with wrong credentials should retun error', function () {
+        before(function () {
+            result = supertest(process.env.BASE_URL)
+                .post('/auth')
+                .send({login: 'wrong', password: 'wrong'})
+        })
+
+        it('response status code is 404', function () {
+            result.expect(404);
+        })
+
+        it('response body contains error message', function () {
+            result.end(function (err, res) {
+                    expect(res.body.message).to.eq('Wrong login or password.');
+                })
+        })
     })
 })
